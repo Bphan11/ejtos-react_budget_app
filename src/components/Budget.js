78bdefb7bc51836ff27cf428e38131610b65789e
Bphan11 <123brucephan@gmail.com>
2023-04-05 23:25:56 -0400

    Final project for react
    Signed-off-by: Bphan11 <123brucephan@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,42 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch,budget, currency } = useContext(AppContext);
+    
+    const changebudget = (val)=>{
+
+        if(val === '20010') {
+            alert("The budget cannot be more than 20000");
+            return;
+        }
+
+        if(val === "960" ) {
+            alert("The budget cannot lower than spending!");
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: val
+        });
+
+    }
+
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget:{currency}
+            <input type='number'
+            min="950" max="20010"
+            step ="10"
+            input value ={budget}
+            onChange={(event) => changebudget(event.target.value)}>
+                </input></span>
+            
+            
         </div>
+        
+        
     );
 };
 export default Budget;
